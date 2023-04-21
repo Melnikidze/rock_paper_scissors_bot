81b5da24a7bc8fec4afbaf7e63acba6d9fb5a350
Melnikidze <46977529+Melnikidze@users.noreply.github.com>
2023-04-21 10:52:20 +0300

    update other_handlers.py

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,1 +1,12 @@
+# import statements
+from aiogram import Router
+from aiogram.types import Message
+from lexicon.lexicon_ru import LEXICON_RU
 
+# router initialization
+router: Router = Router()
+
+# create handler for messages didn't included in another handlers
+@router.message()
+async def send_answer(message: Message):
+    await message.answer(text=LEXICON_RU['other_answer'])
