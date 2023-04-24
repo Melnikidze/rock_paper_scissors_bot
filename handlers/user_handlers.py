0117446fa13e71f8efe766ee505f23c9a73ca453
Melnikidze <46977529+Melnikidze@users.noreply.github.com>
2023-04-24 12:21:04 +0300

    update user_handlers.py

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,1 +1,41 @@
+# import statements
+from aiogram import Router
+from aiogram.filters import Command, CommandStart, Text
+from aiogram.types import Message
+from keyboards.keyboards import yes_no_kb, game_kb
+from lexicon.lexicon_ru import LEXICON_RU
+from services.services import get_bot_choice, get_winner
 
+# router initialization
+router: Router = Router()
+
+# handler for command /start
+@router.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON_RU['/start'], reply_markup=yes_no_kb)
+
+# handler for command /help
+@router.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(text=LEXICON_RU['/help'], reply_markup=yes_no_kb)
+
+# handler for user access to play
+@router.message(Text(text=LEXICON_RU['yes_button']))
+async def process_yes_answer(message: Message):
+    await message.answer(text=LEXICON_RU['yes'], reply_markup=game_kb)
+
+# handler for user decline to play
+@router.message(Text(text=LEXICON_RU['no_button']))
+async def process_no_answer(message: Message):
+    await message.answer(text=LEXICON_RU['no'])
+
+# handler for any game buttons
+@router.message(Text(text=[LEXICON_RU['rock'],
+                           LEXICON_RU['paper'],
+                           LEXICON_RU['scissors']]))
+async def process_game_button(message: Message):
+    bot_choice = get_bot_choice()
+    await message.answer(text=f'{LEXICON_RU["bot_choice"]} '
+                              f'- {LEXICON_RU[bot_choice]}')
+    winner = get_winner(message.text, bot_choice)
+    await message.answer(text=LEXICON_RU[winner], reply_markup=yes_no_kb)
